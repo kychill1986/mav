777349ba0788adb7f2b4be05246b4c7cbd91abcf
yangyichao <yichao.yang@chipease.com>
2013-02-19 14:30:19 +0800

    add select user

--- src/main/java/com/yang/entity/Users.java
+++ src/main/java/com/yang/entity/Users.java
@@ -1,5 +1,36 @@
 package com.yang.entity;
 
-public class Users {
+import java.io.Serializable;
 
+public class Users implements Serializable {
+	/**   */
+	private static final long serialVersionUID = -5873691974645631359L;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	private Integer id;
+	private String username;
+	private String password;
 }
