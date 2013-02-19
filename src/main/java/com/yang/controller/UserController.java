633c04a7229e10c9d2437bbb14a9eacce6a8c94f
yangyichao <yichao.yang@chipease.com>
2013-02-19 15:00:59 +0800

    add user web

--- src/main/java/com/yang/controller/UserController.java
+++ src/main/java/com/yang/controller/UserController.java
@@ -1,5 +1,26 @@
 package com.yang.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.yang.service.UserService;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+	@Resource
+	private UserService userService;
+
+	@RequestMapping(value = "/list")
+	public ModelAndView getUserList() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("user/list");
+		mav.addObject("list",userService.getUserInfo());
+		return mav;
+	}
 
 }
