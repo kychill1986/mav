eaed4289348d25340a591175ae9711427c24d226
yichao yang <yangchao20020@163.com>
2013-04-10 11:30:57 +0800

    add mvc output json/xml support

---
+++ src/main/java/com/yang/controller/JSONController.java
@@ -0,0 +1,32 @@
+package com.yang.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.yang.entity.Users;
+import com.yang.service.UserService;
+
+@Controller
+@RequestMapping("/json")
+public class JSONController {
+
+	@Resource
+	private UserService userService;
+
+	@RequestMapping(method = RequestMethod.GET, value = "/getAllUser")
+	/**只是原方不动的输出对象*/
+	// @ResponseBody
+	/**要使用“http://localhost:8080/mav/json/getAllUser.xml”形式输出xml，注解必须是@ModelAttribute
+	；“http://localhost:8080/mav/json/getAllUser.json”，输出json对象*/
+	@ModelAttribute
+	public List<Users> getAllUser() {
+		List<Users> list = userService.getAllUser();
+		return list;
+	}
+}
