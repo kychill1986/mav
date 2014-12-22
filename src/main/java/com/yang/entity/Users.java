5d4b15d61d62be5ad9ec339aa97caac58e2f7bfa
kychill1986 <yangchao20020@163.com>
2014-12-22 15:17:51 +0800

    aaaaa

--- src/main/java/com/yang/entity/Users.java
+++ src/main/java/com/yang/entity/Users.java
@@ -3,11 +3,14 @@ package com.yang.entity;
 import java.io.Serializable;
 
 public class Users implements Serializable {
+
 	/**   */
 	private static final long serialVersionUID = -5873691974645631359L;
 
 	private String id;
+
 	private String username;
+
 	private String password;
 
 	public String getId() {
@@ -33,4 +36,10 @@ public class Users implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
+
 }
