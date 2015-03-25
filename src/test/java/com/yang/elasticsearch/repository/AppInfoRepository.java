2c7ac7a1aa1d2fbbe5a2528409dc83e7fa93132a
kychill1986 <yangchao20020@163.com>
2015-03-25 17:14:47 +0800

    Signed-off-by: kychill1986 <yangchao20020@163.com>

--- src/test/java/com/yang/elasticsearch/repository/AppInfoRepository.java
+++ src/test/java/com/yang/elasticsearch/repository/AppInfoRepository.java
@@ -8,6 +8,7 @@ import com.yang.elasticsearch.model.AppInfo;
 
 public interface AppInfoRepository extends ElasticsearchRepository<AppInfo, String> {
 
+	// findByAppName根据app名称获取信息列表,这里的"AppName"是和索引属性对应的命名,不可随意修改，之前改成了findByName后报错
 	Page<AppInfo> findByAppName(String appName, Pageable pageable);
 
 	Page<AppInfo> findByPackageName(String packageName, Pageable pageable);
